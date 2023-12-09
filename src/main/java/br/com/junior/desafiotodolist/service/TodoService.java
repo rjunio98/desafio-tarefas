eafcbd60f03fe6fcc6361f681d7edce4fc5e2b5c
rjunio98 <robledorjunior1@gmail.com>
2023-12-09 13:18:27 -0300

    definição dominio serviço

---
+++ src/main/java/br/com/junior/desafiotodolist/service/TodoService.java
@@ -0,0 +1,39 @@
+package br.com.junior.desafiotodolist.service;
+
+import java.util.List;
+
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import br.com.junior.desafiotodolist.entity.Todo;
+import br.com.junior.desafiotodolist.repository.TodoRepository;
+
+@Service
+public class TodoService {
+
+    private TodoRepository todoRepository;
+
+    public TodoService(TodoRepository todoRepository){
+        this.todoRepository = todoRepository;
+    }
+ 
+    public List<Todo> create(Todo todo){
+        todoRepository.save(todo);
+        return list();
+    }
+
+    public List<Todo> list(){
+        Sort sort = Sort.by("prioridade").descending().and(Sort.by("nome").ascending());
+        return todoRepository.findAll(sort);
+    }
+
+    public List<Todo> update(Todo todo){
+        todoRepository.save(todo);
+        return list();
+    }
+
+    public List<Todo> delete(Long id){
+        todoRepository.deleteById(id);
+        return list();
+    }
+}
